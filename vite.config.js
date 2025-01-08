0befaf8bb3354e1f56c7b6c77be2b50b40fd4a02
Fasilfaz <fazsby2001@gmail.com>
2025-01-08 21:52:17 +0530

    error

--- vite.config.js
+++ vite.config.js
@@ -28,5 +28,17 @@ export default defineConfig({
     hmr: {
       overlay: true,
     },
+    headers: [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline';"
+          }
+        ]
+      }
+    ]
   },
+  base: '/'
 })
