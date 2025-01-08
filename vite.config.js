f344be1fd8ded553b9305cd91c91012e1d7c54ac
Fasilfaz <fazsby2001@gmail.com>
2025-01-08 20:41:04 +0530

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'lucide-react': 'lucide-react/dist/esm/lucide-react'
+    }
+  },
   build: {
     rollupOptions: {
       external: ['framer-motion', 'lucide-react']
